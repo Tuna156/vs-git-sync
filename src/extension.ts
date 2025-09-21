2e14a0a40afa54f4003e22fa2ba0e954bdd03b24
Tuna156 <2027156@scprep.org>
2025-09-21 16:13:14 -0400

    Reload and support the authentication menu

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,38 @@
 import { exec } from "child_process";
+import { promisify } from "util";
 import * as vscode from "vscode";
 
-export function activate(context: vscode.ExtensionContext) {
+const execAsync = promisify(exec);
+
+export async function activate(context: vscode.ExtensionContext) {
     promptAccount();
 
-    const disposable = vscode.commands.registerCommand('vs-git-sync.selectAccount', async () => {
+    const disposable = vscode.commands.registerCommand('vsc-git-sync.selectAccount', async () => {
         promptAccount(true);
     });
 
     context.subscriptions.push(disposable);
+    
+
+    vscode.authentication.onDidChangeSessions(async ({ provider }) => {
+        if(provider.id !== 'github') return;
+
+        const session = await vscode.authentication.getSession('github', ['user:email'], { });
+        if(!session) return;
+
+        setGitConfig(session);
+    });
 }
 
 export function deactivate() { }
+
+async function setGitConfig(session: vscode.AuthenticationSession) {
+    const email = await getGithubEmail(session.accessToken);
+    const setUsername = execAsync(`git config --global user.name ${session.account.label}`);
+    const setEmail = execAsync(`git config --global user.email ${email ?? ''}`); // A bit hacky, but if empty, it should print instead of set.
+
+    return Promise.all([setUsername, setEmail]);
+}
 
 async function promptAccount(ignoreConfirm: boolean = false) {
     if (!ignoreConfirm) {
@@ -23,10 +44,9 @@ async function promptAccount(ignoreConfirm: boolean = false) {
         if (prompt !== "Yes") return;
     }
 
-    const session = await vscode.authentication.getSession('github', ['user:email'], { forceNewSession: true, clearSessionPreference: true });
-    const email = await getGithubEmail(session.accessToken);
-    exec(`git config --global user.name ${session.account.label}`);
-    exec(`git config --global user.email ${email}`);
+    const session = await vscode.authentication.getSession('github', ['user:email'], { clearSessionPreference: true, createIfNone: true });
+    await setGitConfig(session);
+    vscode.commands.executeCommand('workbench.action.reloadWindow');
 }
 
 type EmailsResponse = { email: string, primary: boolean }[];
